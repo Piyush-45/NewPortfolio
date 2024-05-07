9c55ac03ed00610d315e2b86e009a8c75f855e5b
Piyush Tyagi <tyagipiyush2233@gmail.com>
2024-05-08 00:43:41 +0530

    draft one

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+// !importing images
+import git from "../images/swiper_webp/git webp.webp";
+import twitter from "../images/swiper_webp/twitter-webp.webp";
+import linkedin from "../images/swiper_webp/linkdin-webp.webp";
+import profilePhoto from "../images/profile.svg";
+import youtube from "../images/youicons.png";
+import { motion } from "framer-motion";
+// !importing css
+
+const Sidebar = () => {
+  return (
+    <motion.aside
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className="top_part">
+        <div className="profile_photo">
+          <img src={profilePhoto} alt="" />
+        </div>
+        <div className="name">Piyush Tyagi</div>
+        <p>Software Engineer | Web & Mobile</p>
+        <ul className="social_icons">
+          <li>
+            <a href="https://github.com/Piyush-45" target="_blank">
+              <motion.img src={git} alt="" whileHover={{scale:1.1}} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/Piyush_ty" target="_blank">
+              <motion.img src={twitter} alt="" whileHover={{scale:1.1}}/>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/piyushtyagidev/"
+              target="_blank"
+            >
+              <motion.img src={linkedin} alt="" whileHover={{scale:1.1}}/>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://youtube.com/@flowingFrontend03?si=Ci_crGwJiHsje2uq"
+              target="_blank"
+            >
+              <motion.img src={youtube} alt="" whileHover={{scale:1.1}}/>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a href="https://drive.google.com/file/d/1dX0aQF_tZr3N0UYCYfq2J_NAilzlv4wy/view?usp=sharing" className="cv_btn">
+        Download CV
+      </a>
+      <div className="copyright">© 2024 All rights reserved.</div>
+    </motion.aside>
+  );
+};
+
+export default Sidebar;
