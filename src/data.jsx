0c1cded71e5b73b4566c4046a180b343a2a216d9
Piyush Tyagi <98485162+Piyush-45@users.noreply.github.com>
2024-07-09 14:31:20 +0530

    web project images fixed

--- src/data.jsx
+++ src/data.jsx
@@ -45,10 +45,10 @@ import ef1 from "../src/images/swiper_webp/ef1.webp";
 import ef2 from "../src/images/swiper_webp/ef3.webp";
 import ef3 from "../src/images/swiper_webp/ef5.webp";
 
-improt tt1 from "../src/images/swiper_webp/tasttango1.png"
-improt tt2 from "../src/images/swiper_webp/tasttango2.png"
-improt tt3 from "../src/images/swiper_webp/tasttango3.png"
-improt tt4 from "../src/images/swiper_webp/tasttango4.png"
+improt tt1 from "../src/images/swiper_webp/tasktango1.png";
+improt tt2 from "../src/images/swiper_webp/tasktango2.png";
+improt tt3 from "../src/images/swiper_webp/tasktango3.png";
+improt tt4 from "../src/images/swiper_webp/tasktango4.png";
 
 export const aboutme = [
   
@@ -149,7 +149,7 @@ export const projects = [
       </>
     )
   },
-  
+
   {
     id: 1,
     image1: `${re1}`,
