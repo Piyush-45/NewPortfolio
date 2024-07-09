94967a5ef95c9a5e991b799e06aaaaf2edbe9da0
Piyush Tyagi <98485162+Piyush-45@users.noreply.github.com>
2024-07-09 14:59:39 +0530

    new app added

--- src/Pages/AppProjects.jsx
+++ src/Pages/AppProjects.jsx
@@ -5,6 +5,7 @@ import aora1 from "../images/Onboarding1.png"
 import aora2 from "../images/aora2.png"
 import aora3 from "../images/aora3.png"
 import aora4 from "../images/aora4.png"
+
 import { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,6 +14,7 @@ import { useInView } from "react-intersection-observer";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import recipeVideo from "../recipeapp.mp4"
 import newsapp from "../newsapp.mp4"
+import  datingapp from "../datingapp.mp4"
 import github_image from "../images/githubf.png"
 import "swiper/css";
 
@@ -41,22 +43,42 @@ const AppProject = () => {
       </h2>
       <div className="project_1">
 
+      <div className="appProjectContainer" >
+          <div className="width">
+            <video width={'100%'} height="360" controls autoPlay >
+              <source src={datingapp} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="description_heading">
+            Zoowaj:Elevate your dating experience.
+          </div>
+        </div>
+        <p className="appDescriptionp">
+        Discover <strong>MatchMate</strong>, my latest freelance project built with React Native and Expo. Effortlessly navigate through matches with a sleek UI and real-time messaging. Powered by <span style={{ color: "#0b9876", fontWeight: 'bold' }}>Supabase</span> and secured with <span style={{ color: "#0b9876", fontWeight: 'bold' }}>Kinde authentication</span>, MatchMate ensures a safe and smooth experience. This app is a testament to my React Native expertise, offering a sophisticated and personalized matchmaking journey. 💖📱✨
+        </p>
 
+        <div className="referal-link">
+          <p style={{ fontSize: '1.3rem', backgroundColor: '#333', padding: '1rem', fontWeight: '300' }}>Source Code : </p>
+          <a href="https://github.com/Piyush-45/Recipe-App" target="_"><img src={github_image} alt="" /></a>
+        </div>
+
+        {/*  */}
         <div className="appProjectContainer" >
           <div className="phoneImages">
             <img src={aora1} className="phoneImg" />
             <img src={aora2} className="phoneImg" />
-            <img src={aora3} className="phoneImg"  />
+            <img src={aora3} className="phoneImg" />
           </div>
           <div className="description_heading">
-          AI Video Hub
+            AI Video Hub
           </div>
         </div>
         <p className="appDescriptionp">
-        Step into the future of video sharing with AI Video Hub – a dynamic React Native application powered by cutting-edge technologies. Seamlessly integrated with the robust backend of Appwrite, and adorned with captivating Animatable animations, this project promises an immersive user experience like never before. With our expertise in <span style={{ color: "#0b9876", fontWeight: 'bold' }}>React Native, Expo, Nativewind, Animatable, and Appwrite </span>, we ensure unparalleled functionality and design. Ready to elevate your app experience? Let's connect and bring your vision to life!
+          Step into the future of video sharing with AI Video Hub – a dynamic React Native application powered by cutting-edge technologies. Seamlessly integrated with the robust backend of Appwrite, and adorned with captivating Animatable animations, this project promises an immersive user experience like never before. With our expertise in <span style={{ color: "#0b9876", fontWeight: 'bold' }}>React Native, Expo, Nativewind, Animatable, and Appwrite </span>, we ensure unparalleled functionality and design. Ready to elevate your app experience? Let's connect and bring your vision to life!
         </p>
 
-        <div className="referal-link" style={{marginBottom:'20px'}}>
+        <div className="referal-link" style={{ marginBottom: '20px' }}>
           <p style={{ fontSize: '1.3rem', backgroundColor: '#333', padding: '1rem', fontWeight: '300' }}>Source Code : </p>
           <a href="https://github.com/alter0313/sora" target="_"><img src={github_image} alt="" /></a>
         </div>
