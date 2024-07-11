4402d681d0af4571c73557e6d57ce4e7ca0c1c5c
Piyush Tyagi <98485162+Piyush-45@users.noreply.github.com>
2024-07-11 12:19:46 +0530

    Update MobileMenu.jsx

--- src/Components/MobileMenu.jsx
+++ src/Components/MobileMenu.jsx
@@ -2,42 +2,73 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import homeIcon from "../images/home f.png";
-import aboutIcons from "../images/about green.png";
+import aboutIcon from "../images/about green.png";
 import servicesIcon from "../images/code f.png";
-import portfolioIcon from "../images/briefcase green.png";
 import contactIcon from "../images/mail.png";
-import blogIcon from "../images/blog green.png"
+import blogIcon from "../images/blog green.png";
+import appdev from "../images/app.png"
+import web from "../images/web.png"
+
 const MobileMenu = () => {
   return (
     <div className="mobile-menu">
-      <div className="home-icon">
+      <div className="icon home ">
+        <div className="tooltip">Home</div>
         <Link to="/">
-          <img src={homeIcon} alt="" />
+          <span>
+            <img src={homeIcon} alt="" />
+          </span>
         </Link>
       </div>
-      <div className="about-icon">
+      <div className="icon about">
+        <div className="tooltip">About</div>
         <Link to="/about">
-          <img src={aboutIcons} alt="" />
+          <span>
+            <img src={aboutIcon} alt="" />
+          </span>
         </Link>
       </div>
-      <div>
+      <div className="icon services">
+        {/* <div className="tooltip"></div> */}
+        <div className="tooltip">Services</div>
         <Link to="/services">
-          <img src={servicesIcon} alt="" />
+          <span>
+            <img src={servicesIcon} alt="" />
+          </span>
         </Link>
       </div>
-      <div>
-        <Link  to="/portfolio">
-          <img src={portfolioIcon} alt="" />
+      <div className="icon blogs">
+        <div className="tooltip">Blogs
+        </div>
+        <Link to="/blogs">
+          <span>
+            <img src={blogIcon} alt="" />
+          </span>
         </Link>
       </div>
-      <div>
-        <Link  to="/blogs">
-          <img src={blogIcon} alt="" />
+      <div className="icon contact">
+        <div className="tooltip">Web Projects</div>
+        <Link to="/portfolio">
+          <span>
+            <img src={web} alt="" />
+          </span>
         </Link>
       </div>
-      <div>
+      <div className="icon contact">
+        <div className="tooltip">App Projects</div>
+        <Link to="/apps">
+          <span>
+            <img src={appdev} alt="" />
+          </span>
+        </Link>
+      </div>
+
+      <div className="icon contact">
+        <div className="tooltip">Contact</div>
         <Link to="/contact">
-          <img src={contactIcon} alt="" />
+          <span>
+            <img src={contactIcon} alt="" />
+          </span>
         </Link>
       </div>
     </div>
